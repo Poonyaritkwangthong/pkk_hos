91b51dd9aa1f25c726870b382dc0d40b67d0bca4
Poonyaritkwangthong <117970215+Poonyaritkwangthong@users.noreply.github.com>
2024-11-22 15:42:40 +0700

    Add files via upload

---
+++ src/app/(page)/dashboard/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import React from 'react'
+import SideBar from '@/app/components/sidebar';
+
+export default function page() {
+  return (
+    <div className="bg-[#f5f5f9] w-screen h-screen p-4">
+      <SideBar/>
+    </div>
+  )
+}
